33c817b0b298b52ca3203b1587439e1023714805
Mukwende <mukwende2000@gmail.com>
2023-05-30 04:30:50 +0200

    test background color

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import styles from './Hero.module.scss'
 function Hero({ heading, bg }: { heading:string, bg: string }) {
   const { pathname } = useLocation()
   return (
-    <section className={`text-secondary ${bg} bg-cover bg-center ${styles.sec}`}>
+    <section className={`$ text-secondary ${bg} bg-[url(../assets/images/logo.png)] bg-cover bg-center ${bg}`}>
         <div className="container py-40">
             { pathname === '/' && <p className="uppercase flex items-center gap-2"><span className="inline-block h-[1px] w-8 bg-primary"></span> welcome to the kaffen</p> }
             <h1 className="w-8/12 md:w-6/12 text-5xl md:text-8xl my-8">{ heading }</h1>
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -3,11 +3,13 @@ import Aboutus from "./components/Aboutus"
 import Hero from '../../components/Hero'
 import Service from "./components/Service"
 import Popular from './components/Popular'
+import text from '../../assets/images/logo.png'
+import styles from './Home.module.scss'
 
 function index() {
   return (
     <>
-      <Hero heading="The London House Cafe" bg="bg-[url('src/assets/images/img34.jpg')]" />
+      <Hero heading="The London House Cafe" bg={styles.home} />
       <Aboutus />
       <div className="container">
         
